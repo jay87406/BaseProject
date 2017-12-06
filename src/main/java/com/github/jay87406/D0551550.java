39be52456eee6fa5b4d4d18060baabade6b3df71
D0551550 <jay8706@gmail.com>
2017-12-06 12:03:02 +0800

    first

---
+++ src/main/java/com/github/jay87406/D0551550.java
@@ -0,0 +1,11 @@
+package com.github.jay87406;
+
+public class D0551550 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+
+        System.out.println("\"Hello " + name + " !\"");
+}
---
+++ src/test/java/com/github/jay87406/D0551550Test.java
@@ -0,0 +1,19 @@
+package com.github.jay87406;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class D0551550Test {
+    @Test
+    public void main() throws Exception {
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            String name = sc.nextLine();
+
+            System.out.println("\"Hello " + name + " !\"");
+    }
+
+}
